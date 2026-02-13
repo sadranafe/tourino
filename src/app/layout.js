86a85ab5f8c8610754ff70a1ba20ad76e2deb4d4
mohammad sadra nafe <sadranafe7@gmail.com>
2026-02-13 11:21:54 +0330

    chore : add metadata to the layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,15 @@ const yekan = localFont({
   src: "./fonts/yekanBakh-Regular.ttf",
 });
 
+export const metadata = {
+  title : {
+    template : '%s tourino',
+    default : 'welcome | tourino'
+  },
+  description: "Your gateway to exclusive online tours. From local hidden gems to international wonders, enjoy curated travel experiences, expert guides, and seamless booking—all online",
+  author : 'sadra nafe',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang = "fa">
