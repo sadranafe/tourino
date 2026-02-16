038a858873adb03ec6ba9e836fb0f92c518e83b8
mohammad sadra nafe <sadranafe7@gmail.com>
2026-02-17 03:02:03 +0330

    style(search) : responsive the UI of the search

--- src/components/SearchTours.js
+++ src/components/SearchTours.js
@@ -52,14 +52,14 @@ const SearchTours = ({ dummyCities }) => {
 
     return (
         <>
-            <div className = "relative border border-dashed rounded-2xl p-1.5 flex justify-center items-center gap-5 mt-5">
+            <div className = "relative max-md:border-0 border max-md:w-full border-dashed rounded-2xl p-1.5 max-md:grid max-md:grid-cols-2 max-[400px]:grid-cols-1 flex flex-wrap justify-center items-center max-md:gap-3 gap-5 mt-5 max-md:px-5">
                 
-                <div className = "flex justify-center items-center border-l pl-3">
+                <div className = "flex justify-center items-center max-md:border max-md:rounded-xl border-l pl-3 max-md:px-0">
                     <label htmlFor = "origin" className = "flex justify-center items-center">
                         <MapPin customClasses = 'text-xl'/>
                     </label>
 
-                    <select name = "origin" id = "origin" value = {origin} onChange = { ev => setOrigin(ev.target.value) } className = "outline-none text-center p-2 px-4 cursor-pointer">
+                    <select name = "origin" id = "origin" value = {origin} onChange = { ev => setOrigin(ev.target.value) } className = "bg-transparent outline-none text-center p-2 px-4 cursor-pointer">
                         <option value = ''>مبدا</option>
                         {
                             dummyCities.map((city , index) => {
@@ -71,12 +71,12 @@ const SearchTours = ({ dummyCities }) => {
                     </select>
                 </div>
 
-                <div className = "flex justify-center items-center border-l pl-3">
+                <div className = "flex justify-center items-center max-md:border max-md:rounded-xl border-l pl-3 max-md:px-0">
                     <label htmlFor = "destination" className = "flex justify-center items-center">
                         <GlobalIcon customClasses = 'text-xl'/>
                     </label>
 
-                    <select name = "destination" id = "destination" value = {destination} onChange = { ev => setDestination(ev.target.value) } className = "outline-none text-center p-2 px-4 cursor-pointer">
+                    <select name = "destination" id = "destination" value = {destination} onChange = { ev => setDestination(ev.target.value) } className = "bg-transparent outline-none text-center p-2 px-4 cursor-pointer">
                         <option value = ''>مقصد</option>
                         {
                             dummyCities.map((city , index) => {
@@ -88,20 +88,29 @@ const SearchTours = ({ dummyCities }) => {
                     </select>
                 </div>
                 
-                <div className = "flex justify-center items-center">
-                    <label htmlFor = "date">
-                        <CalendarDotIcon className = "text-xl"/>
-                    </label>
-                    <DatePicker name = "date" id = "date" value = {selectedDate} onChange = { ev => setSelectedDate(ev) } calendar = {persian} locale = {persian_fa} className = "green" animations = {[opacity()]} calendarPosition = "bottom-center" inputClass = "p-2.5 outline-none rounded-xl" placeholder = "تاریخ"/>
-                </div>
-                
-                <div>
-                    <Link href = {{ pathname : '/' , query }} scroll = { false } onClick = {searchLinkHandler} className = "bg-green-500 hover:bg-green-600 transition-all p-3 block w-40 text-center outline-none text-white rounded-xl">جستوجو</Link>
+                <div className = "flex max-md:flex-wrap max-md:col-span-2 max-[400px]:col-span-1 max-md:gap-2 justify-between items-center">
+                    <div className = "max-md:w-full flex justify-center items-center max-md:border max-md:rounded-xl max-md:text-center max-md:px-0">
+                        <label htmlFor = "date">
+                            <CalendarDotIcon className = "text-xl"/>
+                        </label>
+                        <DatePicker name = "date" id = "date" value = {selectedDate} onChange = { ev => setSelectedDate(ev) } calendar = {persian} locale = {persian_fa} className = "green" animations = {[opacity()]} calendarPosition = "bottom-center" inputClass = "p-2.5 max-[400px]:px-0 bg-transparent outline-none rounded-xl max-md:text-center" placeholder = "تاریخ"/>
+                    </div>
+                    
+                    <div className = {`${hasFilters ? 'max-md:w-9/12' : 'max-md:w-full'} transition-all`}>
+                        <Link href = {{ pathname : '/' , query }} scroll = { false } onClick = {searchLinkHandler} className = "max-md:w-full bg-green-500 hover:bg-green-600 transition-all p-3 block w-40 text-center outline-none text-white rounded-xl">جستوجو</Link>
+                    </div>
+                    
+                    {
+                        hasFilters ?
+                        <div className = "hidden max-md:block max-md:w-[21%] max-[260px]:min-w-fit bg-red-50">
+                            <button onClick = {clearFiltersHandler} className = "w-full max-md:text-xs max-[400px]:text-[10px] border border-dashed border-red-400 hover:bg-red-50 transition-all p-3 px-4 text-center rounded-xl">حذف فیلتر ها</button>
+                        </div> : null
+                    }
                 </div>
 
                 {
                     hasFilters ? 
-                    <div className = "absolute -left-28">
+                    <div className = "max-md:hidden absolute -left-28 lg:top-0 md:-top-14 max-lg:left-0 max-md:bottom-0">
                         <button onClick = {clearFiltersHandler} className = "border border-dashed border-red-400 hover:bg-red-50 transition-all p-3 px-4 text-center rounded-xl">حذف فیلتر ها</button>
                     </div> : null
                 }
