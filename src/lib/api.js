1ce4c780bc044b593a5fced27d228d5886140d8b
mohammad sadra nafe <sadranafe7@gmail.com>
2026-02-25 20:33:49 +0330

    refactor : config the axios again

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,19 +1,58 @@
-import axios from 'axios';
+import axios from "axios";
+import { getCookie, setCookie } from "./cookies";
 
 const api = axios.create({
-    baseURL : 'http://localhost:6500',
-    headers : {
-        "Content-Type": "application/json",
-    },
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.response.use(res => res , async (err) => {
-    if(err.response?.status === 401){
-        console.log('UnAuthorized!');
+api.interceptors.request.use(
+    req => {
+        const accessToken = getCookie('accessToken');
+        if(accessToken) {
+            req.headers['Authorization'] = `Bearer ${accessToken}`;
+        }
+        return req;
+    },
+    err => {
+        return Promise.reject(err);
+    },
+);
+
+api.interceptors.response.use(
+    res => { return res },
+    async (err) => {
+        const originalRequest = err.config;
+        if(err.response.status === 401 && !originalRequest._retry){
+            originalRequest._retry = true;
+        }
+
+        const res = await getNewTokens();
+        if(res?.response?.status === 201) {
+            setCookie('accessToken' , res?.response?.data?.accessToken , 30);
+            setCookie('refreshToken' , res?.response?.data?.refreshToken , 360);
+            return api(originalRequest);
+        } else {
+            setCookie('accessToken' , '' , 0);
+            setCookie('refreshToken' , '' , 0);
+        }
+        return Promise.reject(err.response.data);
     }
+)
 
-    return Promise.reject(err)
-})
 
+const getNewTokens = async () => {
+    const refreshToken = getCookie('refreshToken');
+    if(!refreshToken) return;
+
+    try{
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/refresh-token` , { refreshToken });
+        return { response };
+    } catch (err) {
+        return { err };
+    }
+};
 
 export default api;
