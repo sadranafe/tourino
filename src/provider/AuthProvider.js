081380c0eb53e32f76e6fd8e85bb79469db1b3bf
mohammad sadra nafe <sadranafe7@gmail.com>
2026-02-27 18:07:14 +0330

    feat(auth) : implement authContext with userState and login/logout methods

--- src/provider/AuthProvider.js
+++ src/provider/AuthProvider.js
@@ -1,26 +1,52 @@
 'use client';
 import { AuthContext } from "@/context/AuthContext";
-import { getCookie, setCookie } from "@/utils/cookie";
+import api from "@/lib/api";
+import { deleteCookie, getCookie } from "@/utils/cookie";
 import { useEffect, useState } from "react";
 
 
 const AuthProvider = ({ children }) => {
+    const [user , setUser] = useState(null);
     const [isAuthenticated , setIsAuthenticated] = useState(false);
+    const [loading , setLoading] = useState(true);
 
     useEffect(() => {
         const token = getCookie('accessToken');
-        setIsAuthenticated(!!token);
+        if(!token){
+            setLoading(false);
+            return;
+        }
+
+        fetchUser()
     },[])
 
+    const fetchUser = async () => {
+        try{
+            const res = await api.get('/user/profile');
+            setUser(res.data);
+            setIsAuthenticated(true);
+        } catch {
+            logout()
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const login = userData => {
+        setUser(userData);
+        setIsAuthenticated(true);
+    }
+
     const logout = () => {
-        setCookie('accessToken' , '' , 0);
-        setCookie('refreshToken' , '' , 0);
+        deleteCookie('accessToken');
+        deleteCookie('refreshToken');
         setIsAuthenticated(false);
+        setUser(null);
         window.location.href = '/';
     }
 
     return (
-        <AuthContext.Provider value = {{ isAuthenticated , logout }}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value = {{ user , isAuthenticated , login , logout , loading }}>{children}</AuthContext.Provider>
     )
 };
 
