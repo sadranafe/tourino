753b6f65c45041256bf32637d0a7ead0525173e2
mohammad sadra nafe <sadranafe7@gmail.com>
2026-03-02 00:49:13 +0330

    feat : responsive the profile/account route ui

--- src/app/(protected)/layout.js
+++ src/app/(protected)/layout.js
@@ -17,25 +17,25 @@ const ProtectedLayout = ({ children }) => {
     },[])
 
     return(
-        <div className = "flex justify-between gap-5 p-5 px-10 w-[1280px] mx-auto max-xl:p-5">
-            <div className = "sticky top-5 border border-dashed rounded-xl overflow-hidden max-h-fit w-2/12">
-                <Link href = '/profile' className = "hover:bg-neutral-50 gap-1 hover:text-green-500 transition-all flex justify-start items-center p-3 border-b w-full">
+        <div className = "flex justify-between max-[700px]:flex-wrap gap-5 p-5 px-10 max-lg:px-5 xl:w-[1280px] mx-auto">
+            <div className = "sticky max-[400px]:text-xs bg-white max-[700px]:w-full max-[700px]:flex max-[700px]:justify-center max-[700px]:items-center max-[700px]:shadow-lg max-[700px]:shadow-neutral-50 top-5 border border-dashed rounded-xl overflow-hidden max-h-fit w-[200px]">
+                <Link href = '/profile' className = "hover:bg-neutral-50 gap-1 hover:text-green-500 transition-all flex justify-start items-center p-3 border-b max-[700px]:border-l max-[700px]:border-b-0 w-full max-[700px]:justify-center">
                     <UserIconComponent customClassName = 'w-[18px] text-[15px]'/>
                     پروفایل
                 </Link>
 
-                <Link href = '/profile/my-tour' className = "hover:bg-neutral-50 gap-1 hover:text-green-500 transition-all flex justify-start items-center p-3 border-b w-full">
+                <Link href = '/profile/my-tour' className = "hover:bg-neutral-50 gap-1 hover:text-green-500 transition-all flex justify-start items-center p-3 border-b max-[700px]:border-l max-[700px]:border-b-0 w-full max-[700px]:justify-center">
                     <SunHorizonIcon size = {18} weight = "light"/>
                     تور های من
                 </Link>
                 
-                <Link href = '/profile/transactions' className = "hover:bg-neutral-50 gap-1 hover:text-green-500 transition-all flex justify-start items-center p-3 w-full">
+                <Link href = '/profile/transactions' className = "hover:bg-neutral-50 gap-1 hover:text-green-500 transition-all flex justify-start items-center p-3 w-full max-[700px]:justify-center">
                     <SwapIcon size = {18} weight = "light"/>
                     تراکنش ها
                 </Link>
             </div>
 
-            <div className = "w-10/12 bord r">{ children }</div>
+            <div className = "w-10/12 max-[700px]:w-full">{ children }</div>
         </div>
     )
 }
--- src/app/(protected)/profile/account/page.js
+++ src/app/(protected)/profile/account/page.js
@@ -8,20 +8,20 @@ const AccountProfilePage = () => {
         <>
             <div className = "border rounded-xl p-5">
                 <h1 className = "text-xl">اطلاعات کاربری</h1>
-                <div className = "flex justify-between items-center pr-5 mt-2">
-                    <div className = "flex justify-start w-full items-center">
-                        <div className = "w-1/2">
+                <div className = "flex justify-between items-center max-sm:flex-wrap pr-5 mt-2">
+                    <div className = "flex max-[400px]:flex-wrap max-[400px]:gap-3 max-[400px]:my-3 justify-start max-[870px]:justify-between w-full items-center">
+                        <div className = "w-1/2 max-[870px]:w-fit">
                             <label htmlFor = "phoneNumber" className = "text-neutral-500">شماره موبایل : </label>
-                            <input type = "text" value = {user?.mobile} disabled = {true} id = "phoneNumber" name = "phoneNumber" placeholder = "09109919520" className = "outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
+                            <input type = "text" value = {user?.mobile} disabled = {true} id = "phoneNumber" name = "phoneNumber" placeholder = "09109919520" className = "bg-blue-100 outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
                         </div>
 
                         <div>
                             <label htmlFor = "email" className = "text-neutral-500">ایمیل : </label>
-                            <input type = "text" value = {user?.email || '-'} disabled = {true} id = "email" name = "email" placeholder = "exmaple@gmail.com" className = "outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
+                            <input type = "text" value = {user?.email || '-'} disabled = {true} id = "email" name = "email" placeholder = "exmaple@gmail.com" className = "bg-blue-100 outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
                         </div>
                     </div>
 
-                    <button className = "flex justify-center items-center p-2 text-sky-500 hover:text-sky-700 gap-1">
+                    <button className = "flex justify-center items-center p-2 text-sky-500 hover:text-sky-700 gap-1 max-sm:border max-sm:mt-2 max-sm:hover:bg-neutral-50 border-neutral-100 max-sm:w-full rounded-md">
                         <PencilSimpleLineIcon weight = "light"/> ویرایش
                     </button>
                 </div>
@@ -29,25 +29,25 @@ const AccountProfilePage = () => {
             
             <div className = "border rounded-xl p-5 my-5">
                 <h1 className = "text-xl">اطلاعات فردی</h1>
-                <div className = "flex justify-between items-center pr-5 mt-2">
-                    <div className = "grid grid-cols-2 w-full items-center">
-                        <div>
+                <div className = "flex justify-between max-sm:flex-wrap items-center pr-5 mt-2">
+                    <div className = "grid grid-cols-2 max-[400px]:grid-cols-1 gap-2 max-[400px]:gap-3 max-[400px]:my-3 w-full items-center">
+                        <div className = "max-[925px]:flex max-[925px]:flex-col max-[925px]:justify-center">
                             <label htmlFor = "fullName" className = "text-neutral-500">نام و نام خانوادگی : </label>
                             <input type = "text" value = {user?.firstName && user?.lastName ? `${user?.firstName} ${user?.lastName}` : '-'} disabled = {true} id = "fullName" name = "fullName" className = "outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
                         </div>
 
-                        <div>
+                        <div className = "max-[925px]:flex max-[925px]:flex-col max-[925px]:justify-center">
                             <label htmlFor = "nationalCode" className = "text-neutral-500">کد ملی : </label>
                             <input type = "text" value = {user?.nationalCode || '-'} disabled = {true} id = "nationalCode" name = "nationalCode" placeholder = "012345678" className = "outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
                         </div>
 
-                        <div>
+                        <div className = "max-[925px]:flex max-[925px]:flex-col max-[925px]:justify-center">
                             <label htmlFor = "birthDate" className = "text-neutral-500">تاریخ تولد : </label>
                             <input type = "text" value = {user?.birthDate || '-'} disabled = {true} id = "birthDate" name = "birthDate" className = "outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
                         </div>
                         
                         <div>
-                            <label htmlFor = "gender" className = "text-neutral-500">جنسیت : </label>
+                            <label className = "text-neutral-500">جنسیت : </label>
                             <select name = "gender" id = "gender" className = "bg-transparent outline-none cursor-pointer disabled:cursor-not-allowed">
                                 <option value = "male">مرد</option>
                                 <option value = "woman">زن</option>
@@ -55,7 +55,7 @@ const AccountProfilePage = () => {
                         </div>
                     </div>
 
-                    <button className = "flex justify-center items-center p-2 text-sky-500 hover:text-sky-700 gap-1">
+                    <button className = "flex justify-center items-center p-2 text-sky-500 hover:text-sky-700 gap-1 max-sm:border max-sm:mt-3 max-sm:hover:bg-neutral-50 border-neutral-100 max-sm:w-full rounded-md">
                         <PencilSimpleLineIcon weight = "light"/> ویرایش
                     </button>
                 </div>
@@ -63,8 +63,8 @@ const AccountProfilePage = () => {
             
             <div className = "border rounded-xl p-5">
                 <h1 className = "text-xl">اطلاعات بانکی</h1>
-                <div className = "flex justify-between items-center pr-5 mt-2">
-                    <div className = "grid grid-cols-2 w-full items-center">
+                <div className = "flex justify-between max-sm:flex-wrap items-center pr-5 mt-2">
+                    <div className = "grid grid-cols-2 max-[400px]:grid-cols-1 max-[400px]:gap-3 max-[400px]:my-3 w-full items-center">
                         <div>
                             <label htmlFor = "debtCardCode" className = "text-neutral-500">شماره کارت : </label>
                             <input type = "text" value = {user?.firstName && user?.lastName ? `${user?.firstName} ${user?.lastName}` : '-'} disabled = {true} id = "debtCardCode" name = "debtCardCode" className = "outline-none p-2 mr-2 border border-neutral-50 bg-neutral-50/20 rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.03)] disabled:shadow-none disabled:bg-transparent disabled:border-transparent"/>
@@ -76,7 +76,7 @@ const AccountProfilePage = () => {
                         </div>
                     </div>
 
-                    <button className = "flex justify-center items-center p-2 text-sky-500 hover:text-sky-700 gap-1">
+                    <button className = "flex justify-center items-center p-2 text-sky-500 hover:text-sky-700 gap-1 max-sm:border max-sm:mt-2 max-sm:hover:bg-neutral-50 border-neutral-100 max-sm:w-full rounded-md">
                         <PencilSimpleLineIcon weight = "light"/> ویرایش
                     </button>
                 </div>
