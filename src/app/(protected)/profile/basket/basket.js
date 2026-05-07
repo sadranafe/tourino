2def9571babe0fa092e2907b5bd10f8f49444103
mohammad sadra nafe <sadranafe7@gmail.com>
2026-05-07 17:36:58 +0330

    style : edit the UI of the basket page

--- src/app/(protected)/profile/basket/basket.js
+++ src/app/(protected)/profile/basket/basket.js
@@ -1,21 +1,39 @@
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useMutation } from "@tanstack/react-query";
+import api from "@/lib/api";
+import { calculateTourDuration } from "@/helper/helper";
+import toast from "react-hot-toast";
+import PassengerInfo from "@/components/PassengerInfo";
 import ChevronLeftIcon from "@/components/icons/ChevLeftIcon";
 import UserIconComponent from "@/components/icons/userIcon";
-import PassengerInfo from "@/components/PassengerInfo";
-import { calculateTourDuration } from "@/helper/helper";
-import Image from "next/image";
 
 const Basket = props => {
-    const { id , image : img , title , price , startDate , endDate , user } = props;
+    const { image : img , title , price , startDate , endDate , user } = props;
     const tourDuration = calculateTourDuration(startDate , endDate);
-
-    const backBtnHandler = () => {
-        console.log('first')
+    const router = useRouter();
+    
+    const mutationFn = () => {
+        const { fullname , nationalCode , birthDate , gender } = user;
+        return api.post('/order' , { fullName : fullname, nationalCode , gender , birthDate })
     }
+
+    const { mutate } = useMutation({
+        mutationKey : ['order'],
+        mutationFn,
+        onSuccess : res => {
+            toast.success(res?.data?.message)
+            // we have to redirect the user to the /profile/transactions and show the user's submitted tours
+        },
+        onError : err => {
+            console.dir('error : ' + err)
+        }
+    })
 
     return (
         <div className = "grid grid-cols-3 max-[845px]:grid-cols-2 max-[500px]:grid-cols-1 gap-2">
             <div className = "relative border rounded-xl p-5 col-span-2 max-[845px]:col-span-1">
-                <button onClick = {backBtnHandler} className = "left-4 top-4 absolute">
+                <button onClick = {() => router.back()} className = "left-4 top-4 absolute hover:bg-neutral-100 rounded-full p-1.5 transition-all">
                     <ChevronLeftIcon customClasses = 'w-5 h-5 rounded-full text-neutral-400 cursor-pointer'/>
                 </button>
                 <div className = "flex justify-normal items-center gap-2 mb-5">
@@ -23,14 +41,14 @@ const Basket = props => {
                     <h1 className = "text-2xl">مشخصات مسافر</h1>
                 </div>
 
-                <div className = "grid grid-cols-2 max-[845px]:grid-cols-1 items-center justify-items-center gap-y-2">
+                <div className = "grid grid-cols-2 max-[845px]:grid-cols-1 items-center justify-items-center gap-y-4">
                     <PassengerInfo { ...user } />
                 </div>
             </div>
 
             <div className = "border rounded-xl col-span-1">
                 <div className = "w-full relative aspect-[16/9]">
-                    <Image src = {img} alt = {title} fill sizes = "100vw" className = "object-cover"/>
+                    <Image src = {img} alt = {title} priority fill sizes = "(max-width: 500px) 100vw, (max-width: 845px) 50vw, 33vw" className = "object-cover"/>
                 </div>
 
                 <div className = "p-2">
@@ -39,7 +57,7 @@ const Basket = props => {
                         <p className = "text-neutral-400">{`${tourDuration?.night} شب  و ${tourDuration?.day} روز`}</p>
                     </div>
 
-                    <div className = "border-t border-dashed border-neutral-300 pt-3 flex justify-between items-center px-2">
+                    <div className = "border-t border-dashed border-neutral-300 pt-3 flex justify-between items-center px-2 my-3">
                         <p>قیمت نهایی </p>
                         <p>
                             <span className = "text-xl max-[1100px]:text-base text-blue-400 ml-1">{price?.toLocaleString()}</span> <span>تومان</span>
@@ -47,7 +65,7 @@ const Basket = props => {
                     </div>
 
                     <div>
-                        <button className = "w-full bg-green-500 hover:bg-green-600 transition-all text-white rounded-md p-2 mt-2">ثبت و خرید نهایی</button>
+                        <button onClick = { () => mutate() } className = "w-full bg-green-500 hover:bg-green-600 transition-all text-white rounded-md p-2 py-3">ثبت و خرید نهایی</button>
                     </div>
                 </div>
 
--- src/components/PassengerInfo.js
+++ src/components/PassengerInfo.js
@@ -1,4 +1,3 @@
-
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian"
 import persian_en from "react-date-object/locales/persian_en"
@@ -8,19 +7,19 @@ const PassengerInfo = props => {
     return (
         <>
             <div className = "w-8/12 max-lg:w-10/12">
-                <input type = "text" placeholder = 'نام و نام خانوداگی' defaultValue = {fullname} disabled className = "p-2 rounded-sm border border-neutral-300 w-full text-center disabled:bg-transparent disabled:cursor-not-allowed"/>
+                <input type = "text" placeholder = 'نام و نام خانوداگی' defaultValue = {fullname} disabled className = "py-3 rounded-md border border-neutral-200 w-full text-center disabled:bg-transparent disabled:cursor-not-allowed"/>
             </div>
 
             <div className = "w-8/12 max-lg:w-10/12">
-                <input type = "text" placeholder = 'کد ملی' defaultValue = {nationalCode} disabled className = "p-2 rounded-sm border border-neutral-300 w-full text-center disabled:bg-transparent disabled:cursor-not-allowed"/>
+                <input type = "text" placeholder = 'کد ملی' defaultValue = {nationalCode} disabled className = "py-3 rounded-md border border-neutral-200 w-full text-center disabled:bg-transparent disabled:cursor-not-allowed"/>
             </div>
 
             <div className = "passenger-birthDate w-8/12 max-lg:w-10/12">
-                <DatePicker name = "birthDate" id = 'birthDate' placeholder = "تاریخ تولد" value = {birthDate} disabled calendarPosition = "bottom-center" calendar = {persian} locale = {persian_en} inputClass = 'bg-transparent disabled:cursor-not-allowed outline-none outline-none border border-neutral-300 p-2 rounded-md w-full text-center'/>
+                <DatePicker name = "birthDate" id = 'birthDate' placeholder = "تاریخ تولد" value = {birthDate} disabled calendarPosition = "bottom-center" calendar = {persian} locale = {persian_en} inputClass = 'bg-transparent disabled:cursor-not-allowed outline-none outline-none border border-neutral-200 py-3 rounded-md w-full text-center'/>
             </div>
 
             <div className = "w-8/12 max-lg:w-10/12">
-                <select name = "gender" id = "gender" defaultValue = {gender} disabled className = "bg-transparent p-2 rounded-sm border border-neutral-300 w-full text-center disabled:cursor-not-allowed ">
+                <select name = "gender" id = "gender" defaultValue = {gender} disabled className = "bg-transparent py-3 rounded-md border border-neutral-200 w-full text-center disabled:cursor-not-allowed ">
                     <option value = 'male'>مرد</option>
                     <option value = 'female'>زن</option>
                 </select>
