e781dad966b96e84501e15254e872d3a3a45e387
sadra <sadranafe7@gmail.com>
2026-05-30 02:08:59 +0330

    Enable localhost image remote pattern
    Uncomment localhost remote pattern for images.

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,13 @@
 const nextConfig = {
     images : {
         remotePatterns : [
-            // {
-            //     protocol : 'http',
-            //     hostname : 'localhost',
-            //     // port : '6500',
-            //     port : '8080',
-            //     pathname : '/**',
-            // },
+            {
+                protocol : 'http',
+                hostname : 'localhost',
+                // port : '6500',
+                port : '8080',
+                pathname : '/**',
+            },
             {
                 protocol : 'https',
                 hostname : 'vigilant-wholeness-production-18fc.up.railway.app',
