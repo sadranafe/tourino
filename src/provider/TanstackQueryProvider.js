e8df6d014c3b291a561282caeabf86781989765b
mohammad sadra nafe <sadranafe7@gmail.com>
2026-06-02 03:08:20 +0330

    fix : handle truncated API response and prevent infinite retry loop

--- src/provider/TanstackQueryProvider.js
+++ src/provider/TanstackQueryProvider.js
@@ -1,7 +1,13 @@
 'use client';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+    defaultOptions : {
+        queries : {
+            retry : 3,
+        }
+    }
+});
 
 const TanstackQueryProvider = ({ children }) => {
     return <QueryClientProvider client = { queryClient }>{ children }</QueryClientProvider>
